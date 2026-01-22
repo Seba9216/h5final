e49409fb82ab12d279ef7b9152659e2966ccb2f4
August2211 <augustneustrup@hotmail.com>
2026-01-22 10:47:46 +0100

    Fixed ducks

--- WebApp/h5-webapp-pwa/src/app/pages/home-page/home-page.ts
+++ WebApp/h5-webapp-pwa/src/app/pages/home-page/home-page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-   selector: 'app-home-page',
+  selector: 'app-home-page',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './home-page.html', 
-
+  templateUrl: './home-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePage implements OnInit {
   raceStarted = false;
+
   ducks = [
     { id: 1, speed: 0, color: '#fbe405' },
     { id: 2, speed: 0, color: '#ff5733' },
@@ -18,35 +24,35 @@ export class HomePage implements OnInit {
     { id: 5, speed: 0, color: '#f033ff' }
   ];
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
   ngOnInit() {
     this.startRaceCycle();
   }
 
   startRaceCycle() {
-    // 1. Assign random speeds
     this.ducks = this.ducks.map(duck => ({
       ...duck,
-      speed: Math.random() * (6 - 3) + 3 // Speeds between 3s and 6s
+      speed: Math.random() * (6 - 3) + 3
     }));
+    this.cdr.markForCheck();
 
-    // 2. Start the race
     setTimeout(() => {
       this.raceStarted = true;
-      
-      // 3. Find the slowest duck to know when the race is "over"
+      this.cdr.markForCheck();
+
       const slowestTime = Math.max(...this.ducks.map(d => d.speed));
 
-      // 4. Wait for the race to finish + a small pause to celebrate
       setTimeout(() => {
         this.resetRace();
-      }, (slowestTime * 1000) + 2000); 
-
-    }, 100); 
+      }, slowestTime * 1000 + 2000);
+    }, 100);
   }
 
   resetRace() {
     this.raceStarted = false;
-    // 5. Wait for the CSS reset transition (1.2s) to finish before restarting
+    this.cdr.markForCheck();
+
     setTimeout(() => {
       this.startRaceCycle();
     }, 1500);
