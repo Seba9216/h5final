1f76d02407754f670acdc5bbeb5087b10916423a
Sebastian Veggerby Rasmussen (SFU) <SFU@kmd.dk>
2026-01-27 12:57:12 +0100

    remove commit

--- WebSocketServer/WebSocketServer.Server/Core/Connections/IWebSocketHandler.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Http;
-using System.Net.WebSockets;
-
-namespace WebSocketServer.Core.Handlers;
-
-public interface IWebSocketHandler
-{
-    Task HandleAsync(HttpContext context, WebSocket webSocket);
-}
