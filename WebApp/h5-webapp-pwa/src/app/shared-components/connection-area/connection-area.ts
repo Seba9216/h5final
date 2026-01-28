7ebba8b7121fda1eb6d207858bb59d28221ccba6
August2211 <augustneustrup@hotmail.com>
2026-01-28 09:36:35 +0100

    New game code is shown on game creation

--- WebApp/h5-webapp-pwa/src/app/shared-components/connection-area/connection-area.ts
+++ WebApp/h5-webapp-pwa/src/app/shared-components/connection-area/connection-area.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-connection-area',
@@ -7,48 +7,53 @@ import { Component } from '@angular/core';
   styleUrl: './connection-area.scss',
 })
 export class ConnectionArea {
- private SERVER_URL = "ws://localhost:5057/ws";
-   gamePin = '';
-   duckerName = '';
+  private SERVER_URL = "ws://localhost:5057/ws";
+  gamePin = '';
+  duckerName = '';
+  newGamePin = '';
 
+    constructor(
+    private cdr: ChangeDetectorRef
+  ) {}
 
- public CreateGame(){
-  const ws = new WebSocket(this.SERVER_URL);
-  let lobbyCode = null;
-ws.addEventListener("open", () => {
-    ws.send(JSON.stringify({
+  public CreateGame() {
+    const ws = new WebSocket(this.SERVER_URL);
+    let lobbyCode: string = "";
+    ws.addEventListener("open", () => {
+      ws.send(JSON.stringify({
         type: "create_lobby"
-    }));
-});
-ws.addEventListener("message", (data) => {
-    console.log(data.data);
-    const message = JSON.parse(data.data.toString());
-    if (message.type === "lobby_created") {
-        lobbyCode = message.lobby_code;
+      }));
+    });
+    ws.addEventListener("message", (data) => {
+      console.log(data.data);
+      const message = JSON.parse(data.data.toString());
+      if (message.Type === "lobby_created") {
+        lobbyCode = message.LobbyCode;
 
         console.log("Lobby created:", lobbyCode);
-    }
-});
+        this.newGamePin = lobbyCode;
+        this.cdr.detectChanges();
+      }
+    });
 
- }
+  }
 
- public JoinGame(){  const ws = new WebSocket(this.SERVER_URL);
-  const lobbyCode: number = +this.gamePin;
-ws.addEventListener("open", () => {
- 
-ws.send(JSON.stringify({
-     type: "join_lobby",
-     LobbyCode: lobbyCode,
-     DuckerName: this.duckerName
- }));
-});
-ws.addEventListener("message", (data) => {
-    console.log(data.data);
-    const message = JSON.parse(data.data.toString());
-    console.log("Received:", message);
+  public JoinGame() {
+    const ws = new WebSocket(this.SERVER_URL);
+    const lobbyCode: number = +this.gamePin;
+    ws.addEventListener("open", () => {
 
-});
-
- }
-
+      ws.send(JSON.stringify({
+        type: "join_lobby",
+        LobbyCode: lobbyCode,
+        DuckerName: this.duckerName
+      }));
+    });
+    ws.addEventListener("message", (data) => {
+      console.log(data.data);
+      const message = JSON.parse(data.data.toString());
+      console.log("Received:", message);
+      
+    });
+  }
 }
