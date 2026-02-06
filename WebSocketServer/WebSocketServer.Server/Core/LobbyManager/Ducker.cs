1bfb47e99dfdc8d0686792b697f511bdff40d357
August <augustneustrup@hotmail.com>
2026-02-06 10:59:23 +0100

    C# er et helt fint sprog uden mærkelige ting eller problemer

--- WebSocketServer/WebSocketServer.Server/Core/LobbyManager/Ducker.cs
+++ WebSocketServer/WebSocketServer.Server/Core/LobbyManager/Ducker.cs
@@ -1,5 +1,11 @@
 namespace WebSocketServer.Core.LobbyManager;
 
+using System.Text.Json.Serialization;
+
+
+[JsonDerivedType(typeof(Ducker), typeDiscriminator: "base")]
+[JsonDerivedType(typeof(RacerDucker), typeDiscriminator: "racer")]
+[JsonDerivedType(typeof(PokerDucker), typeDiscriminator: "poker")]
 public abstract class Ducker
 {
     public string ConnectionId { get; set; }
