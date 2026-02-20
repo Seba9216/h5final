606c569a106ecabf480e91d7a25bbf9eb65e367b
Sebastian Veggerby Rasmussen (SFU) <SFU@kmd.dk>
2026-02-20 11:04:43 +0100

    mysql

--- WebSocketServer/WebSocketServer.Server/Program.cs
+++ WebSocketServer/WebSocketServer.Server/Program.cs
@@ -4,7 +4,7 @@ using WebSocketServer.Core.context;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DuckingContext>
- (options => options.UseMySQL("server=localhost;database=testdb;user=user;password=password;",
+ (options => options.UseMySQL("server=mysql;database=testdb;user=user;password=password;",
     b => b.MigrationsAssembly("WebSocketServer.Server")));
 var startup = new Startup();
 startup.ConfigureServices(builder.Services);
