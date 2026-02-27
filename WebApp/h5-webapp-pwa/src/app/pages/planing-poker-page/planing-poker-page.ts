bf30f38daf1be326d5b170328df81c1ffbc129f2
Sebastian Veggerby Rasmussen (SFU) <SFU@kmd.dk>
2026-02-27 08:09:01 +0100

    game ended corectly for host also

--- WebApp/h5-webapp-pwa/src/app/pages/planing-poker-page/planing-poker-page.ts
+++ WebApp/h5-webapp-pwa/src/app/pages/planing-poker-page/planing-poker-page.ts
@@ -51,7 +51,7 @@ ngOnInit() {
   this.socketService.gameEndend$.subscribe(() => {
     this.gameHasEnded = true;
     this.winnerModal.closeureText = "We have an agreement!"
-    this.winnerModal.duckerName = "The task is estimated at " + this.players[0].storyPoints;
+    this.winnerModal.duckerName = "The " + this.task + " is estimated at " + this.players[0].storyPoints;
     this.winnerModal.show();
     this.socketService.closeWebSocketConnection();
     this.cdr.markForCheck();
@@ -108,7 +108,7 @@ ngOnInit() {
   this.players.every(p => p.storyPoints === this.players[0].storyPoints);
   if(allSameStoryPoints){
     this.socketService.gameFinished();
-    this.winnerModal.show();
+  this.gameHasEnded = true;
   } 
   }
   newRound() {
